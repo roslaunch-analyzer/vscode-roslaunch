a0d6772719cfc241c8fcddbe829a903ddde432e2
Y.Hisaki <yhisaki31@gmail.com>
2024-06-24 19:02:25 +0900

    add warning if roslaunch-language-server is not installed
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import {
 } from 'vscode-languageclient/node';
 import * as net from 'net';
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { exec } from 'child_process';
 import { VisualizerPanel } from "./panels/launch_tree/VisualizerPanel";
 import { getWebviewContent, LaunchFileParameters, getChangedParameters } from "./panels/launch_tree/ParameterChoice";
 
@@ -44,146 +45,164 @@ export async function activate(context: vscode.ExtensionContext) {
         host: "localhost"
     };
 
-    proc = spawn("roslaunch-language-server", ["--port", String(port)], {
-        env: env
-    });
-
-    proc.stdout.on("data", (data) => {
-        console.log(`stdout: ${data}`);
-    });
-    proc.stderr.on("data", (data) => {
-        console.error(`stderr: ${data}`);
-    });
-    proc.on("close", (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-    proc.on("error", (err) => {
-        console.error(`Failed to spawn process: ${err.message}`);
-    });
-    proc.on("spawn", () => {
-        console.log("Process spawned successfully.");
-    });
-
-    await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 2 seconds to ensure the server has time to start
-
-    let serverOptions = () => {
-        return new Promise<StreamInfo>((resolve, reject) => {
-            let socket = net.connect(connectionInfo, () => {
-                console.log("Connected to server");
-                resolve({
-                    writer: socket,
-                    reader: socket
-                });
-            }).on('error', (err) => {
-                console.error(`Could not connect to server: ${err.message}`);
-                reject(err);
+    // Check if roslaunch-language-server is executable
+    exec("command -v roslaunch-language-server", (error, stdout, stderr) => {
+        if (error) {
+            console.error("roslaunch-language-server is not executable.");
+            vscode.window.showInformationMessage(
+                "roslaunch-language-server is not installed. Please install it from https://github.com/roslaunch-analyzer/roslaunch-language-server",
+                "Open Link"
+            ).then(selection => {
+                if (selection === "Open Link") {
+                    vscode.env.openExternal(vscode.Uri.parse("https://github.com/roslaunch-analyzer/roslaunch-language-server"));
+                }
             });
-        });
-    };
-
-    let client = new LanguageClient(
-        "Roslaunch Language Server",
-        "roslaunch-language-server",
-        serverOptions,
-        {
-            documentSelector: [
-                { scheme: 'file', language: 'xml' }
-            ]
+            return;
         }
-    );
 
-    console.log("activate roslaunch-analyze extension");
-    client.start().then(() => {
-        console.log("Client has started");
-    }, (err) => {
-        console.error(`Client failed to start: ${err.message}`);
-    });
+        console.log(`roslaunch-language-server is located at ${stdout.trim()}`);
 
-    client.sendRequest("hello_world", { hello: "world" }).then((response) => {
-        console.log("hello");
-        console.log(response);
-    });
+        proc = spawn("roslaunch-language-server", ["--port", String(port)], {
+            env: env
+        });
 
-    context.subscriptions.push(client);
+        proc.stdout.on("data", (data) => {
+            console.log(`stdout: ${data}`);
+        });
+        proc.stderr.on("data", (data) => {
+            console.error(`stderr: ${data}`);
+        });
+        proc.on("close", (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+        proc.on("error", (err) => {
+            console.error(`Failed to spawn process: ${err.message}`);
+        });
+        proc.on("spawn", () => {
+            console.log("Process spawned successfully.");
+        });
 
-    console.log("Client Activated");
-
-    const openInRosLaunchManager = vscode.commands.registerCommand('vscode-roslaunch.openInRosLaunchManager', async (uri: vscode.Uri) => {
-        const document = await vscode.workspace.openTextDocument(uri);
-        await vscode.window.showTextDocument(document, { viewColumn: vscode.ViewColumn.One });
-
-        const params = { filepath: uri.path };
-        await client.sendRequest<LaunchFileParameters>("get_launch_file_parameters", params).then((parameters) => {
-            console.log("Launch file analysis:", parameters);
-
-            const showParams = JSON.parse(JSON.stringify(parameters));
-            // Apply cached parameters if available
-            const cachedParameters = parameterCache.get(uri.path);
-            if (cachedParameters) {
-                cachedParameters.forEach(([key, val]) => {
-                    if (showParams[key]) {
-                        showParams[key].default = val;
-                    }
+        setTimeout(() => {
+            let serverOptions = () => {
+                return new Promise<StreamInfo>((resolve, reject) => {
+                    let socket = net.connect(connectionInfo, () => {
+                        console.log("Connected to server");
+                        resolve({
+                            writer: socket,
+                            reader: socket
+                        });
+                    }).on('error', (err) => {
+                        console.error(`Could not connect to server: ${err.message}`);
+                        reject(err);
+                    });
                 });
-            }
+            };
 
-            const panel = vscode.window.createWebviewPanel(
-                'editLaunchParameters',
-                'Edit Launch Parameters',
-                vscode.ViewColumn.Two,
+            let client = new LanguageClient(
+                "Roslaunch Language Server",
+                "roslaunch-language-server",
+                serverOptions,
                 {
-                    enableScripts: true,
-                    retainContextWhenHidden: true
+                    documentSelector: [
+                        { scheme: 'file', language: 'xml' }
+                    ]
                 }
             );
 
-            panel.webview.html = getWebviewContent(showParams);
+            console.log("activate roslaunch-analyze extension");
+            client.start().then(() => {
+                console.log("Client has started");
+            }, (err) => {
+                console.error(`Client failed to start: ${err.message}`);
+            });
 
-            panel.webview.onDidReceiveMessage(
-                message => {
-                    switch (message.command) {
-                        case 'updateParameters':
-                            const updatedParameters = message.parameters;
-                            const upd_result = getChangedParameters(parameters, updatedParameters);
+            client.sendRequest("hello_world", { hello: "world" }).then((response) => {
+                console.log("hello");
+                console.log(response);
+            });
 
-                            parameterCache.set(uri.path, upd_result);
+            context.subscriptions.push(client);
 
-                            // Save parameterCache to local storage
-                            context.workspaceState.update('parameterCache', JSON.stringify(Array.from(parameterCache.entries()))).then(() => {
-                                console.log("Parameter cache saved to local storage.");
-                            }, (error) => {
-                                console.error("Error saving parameter cache to local storage:", error);
-                            });
+            console.log("Client Activated");
 
-                            const sendParams = {
-                                colcon_path: env["COLCON_PREFIX_PATH"],
-                                filepath: uri.path,
-                                arguments: upd_result
-                            };
-                            console.log("Updated parameters:", sendParams);
-                            client.sendRequest("parse_launch_file", sendParams).then((treeJson) => {
-                                console.log("Tree: ", treeJson);
-                                panel.dispose();
-                                VisualizerPanel.render(context.extensionUri, vscode.ViewColumn.Two, treeJson);
-                            }).catch((error) => {
-                                console.error("Error analyzing launch file:", error);
-                                panel.webview.postMessage({
-                                    command: 'error',
-                                    error: error.message
-                                });
-                            });
-                            return;
+            const openInRosLaunchManager = vscode.commands.registerCommand('vscode-roslaunch.openInRosLaunchManager', async (uri: vscode.Uri) => {
+                const document = await vscode.workspace.openTextDocument(uri);
+                await vscode.window.showTextDocument(document, { viewColumn: vscode.ViewColumn.One });
+
+                const params = { filepath: uri.path };
+                await client.sendRequest<LaunchFileParameters>("get_launch_file_parameters", params).then((parameters) => {
+                    console.log("Launch file analysis:", parameters);
+
+                    const showParams = JSON.parse(JSON.stringify(parameters));
+                    // Apply cached parameters if available
+                    const cachedParameters = parameterCache.get(uri.path);
+                    if (cachedParameters) {
+                        cachedParameters.forEach(([key, val]) => {
+                            if (showParams[key]) {
+                                showParams[key].default = val;
+                            }
+                        });
                     }
-                },
-                undefined,
-                context.subscriptions
-            );
-        }).catch((error) => {
-            console.error("Error analyzing launch file:", error);
-        });
-    });
 
-    context.subscriptions.push(openInRosLaunchManager);
+                    const panel = vscode.window.createWebviewPanel(
+                        'editLaunchParameters',
+                        'Edit Launch Parameters',
+                        vscode.ViewColumn.Two,
+                        {
+                            enableScripts: true,
+                            retainContextWhenHidden: true
+                        }
+                    );
+
+                    panel.webview.html = getWebviewContent(showParams);
+
+                    panel.webview.onDidReceiveMessage(
+                        message => {
+                            switch (message.command) {
+                                case 'updateParameters':
+                                    const updatedParameters = message.parameters;
+                                    const upd_result = getChangedParameters(parameters, updatedParameters);
+
+                                    parameterCache.set(uri.path, upd_result);
+
+                                    // Save parameterCache to local storage
+                                    context.workspaceState.update('parameterCache', JSON.stringify(Array.from(parameterCache.entries()))).then(() => {
+                                        console.log("Parameter cache saved to local storage.");
+                                    }, (error) => {
+                                        console.error("Error saving parameter cache to local storage:", error);
+                                    });
+
+                                    const sendParams = {
+                                        colcon_path: env["COLCON_PREFIX_PATH"],
+                                        filepath: uri.path,
+                                        arguments: upd_result
+                                    };
+                                    console.log("Updated parameters:", sendParams);
+                                    client.sendRequest("parse_launch_file", sendParams).then((treeJson) => {
+                                        console.log("Tree: ", treeJson);
+                                        panel.dispose();
+                                        VisualizerPanel.render(context.extensionUri, vscode.ViewColumn.Two, treeJson);
+                                    }).catch((error) => {
+                                        console.error("Error analyzing launch file:", error);
+                                        panel.webview.postMessage({
+                                            command: 'error',
+                                            error: error.message
+                                        });
+                                    });
+                                    return;
+                            }
+                        },
+                        undefined,
+                        context.subscriptions
+                    );
+                }).catch((error) => {
+                    console.error("Error analyzing launch file:", error);
+                });
+            });
+
+            context.subscriptions.push(openInRosLaunchManager);
+        }, 5000); // Wait for 5 seconds to ensure the server has time to start
+    });
 }
 
 export function deactivate() {
